3c18dda7bbb92b9cb86d961af10cb6cb46fa8da1
HESHAM47GAMAL <hesham.shehatah00@eng-st.cu.edu.eg>
2024-04-06 22:34:45 +0200

    document user interface for WIFI

--- Eclipse_WS/WIFI_ThingSpeak/HAL/WIFI/WIFI_config.h
+++ Eclipse_WS/WIFI_ThingSpeak/HAL/WIFI/WIFI_config.h
@@ -17,22 +17,23 @@
 
 
 
-#define    WIFI_STATION_mode         "AT+CWMODE_DEF=1\r\n"
-#define    WIFI_API_mode             "AT+CWMODE_DEF=2\r\n"
-#define    WIFI_STATION_API_mode     "AT+CWMODE_DEF=3\r\n"
+#define    WIFI_STATION_mode         "AT+CWMODE_DEF=1\r\n"      //      option1         ---
+#define    WIFI_API_mode             "AT+CWMODE_DEF=2\r\n"      //      option2         ---
+#define    WIFI_STATION_API_mode     "AT+CWMODE_DEF=3\r\n"      //      option3         ---
+//                                                                                          | Here   
+#define WIFI_Current_Mode       WIFI_STATION_API_mode   //                                <--
 
 
-#define WIFI_Current_Mode       WIFI_STATION_API_mode
-
-#define WIFI_Single_Channel_mode        "AT+CIPMUX=0\r\n"
-#define WIFI_Multiple_Channel_mode      "AT+CIPMUX=1\r\n"
-
-#define WIFI_Current_Channel_mode       WIFI_Single_Channel_mode
+#define WIFI_Single_Channel_mode        "AT+CIPMUX=0\r\n"           //      option1         ---
+#define WIFI_Multiple_Channel_mode      "AT+CIPMUX=1\r\n"           //      option2         ---
+//                                                                                             | Here   
+#define WIFI_Current_Channel_mode       WIFI_Single_Channel_mode //                          <--
 
 
-#define WIFI_Transfer_normal_mode               "AT+CIPMODE=0\r\n"
-#define WIFI_Transfer_Passthrough_mode          "AT+CIPMODE=1\r\n"
+#define WIFI_Transfer_normal_mode               "AT+CIPMODE=0\r\n"          //      option1         ---
+#define WIFI_Transfer_Passthrough_mode          "AT+CIPMODE=1\r\n"          //      option2         ---
+//                                                                                                     | Here   
+#define WIFI_Current_Transfer_Mode         WIFI_Transfer_normal_mode     //                         <--
 
-#define WIFI_Current_Transfer_Mode         WIFI_Transfer_normal_mode
 
 #endif
--- Eclipse_WS/WIFI_ThingSpeak/HAL/WIFI/WIFI_interface.h
+++ Eclipse_WS/WIFI_ThingSpeak/HAL/WIFI/WIFI_interface.h
@@ -24,6 +24,8 @@ typedef enum
     WIFI_InProgress = 20
 }WIFI_Result ;
 
+
+/*  user defined data type for connection with Network result   */
 typedef enum
 {
     WIFI_Connection_OK                  = 1 ,
@@ -34,6 +36,7 @@ typedef enum
 }WIFI_SSD_Connection_Type;
 
 
+/*  user defined data type for status of connection with network*/
 typedef enum 
 {
     WIFI_AP_GotIP           = 2,
@@ -42,6 +45,8 @@ typedef enum
     WIFI_AP_Connection_Fail =5
 }WIFI_AP_Status_Type;
 
+
+/*  user defined data type for openning socket (also can called onnection with server)*/
 typedef enum 
 {
     WIFI_Socket_OK                  = 1,
@@ -50,21 +55,64 @@ typedef enum
 }WIFI_Socket_Result_Type ;
 
 
+/*  user defined data type for result of sending data for server */
 typedef enum
 {
     WIFI_SEND_ERROR         = 0,
     WIFI_SEND_OK            =1 
 }WIFI_SEND_Result_Type;
 
+
+/*
+*   @brief : this function used to initailize Wifi Module (with modes that will configured in WIFI_config.h  and also need to go to USART_config.h , .c to configure mode for it)
+*   and also initilaize timer0 as used to make timeout 
+*   arg1 WIFI_state: will carry result for this function that can be any state from this defined data type (WIFI_Result)
+*   @return: no return
+*   @synchronous / Asynchronous : Synchronous
+*   @ Reentrant / Non Reentrant : Reentrant
+*/
 void WIFI_Init(WIFI_Result * WIFI_state);
 
-
+/*
+*   @brief : this function used to initailize LCD  pins like RS , RW , E and Data pins 
+*   @arg1  SSID: pointer for array of characters for your network name
+*   @arg2  pass: pointer for array of characters for password of your network
+*   @arg3  pRresult:  will carry result for this function that can be any state from this defined data type (WIFI_SSD_Connection_Type)
+*   @return: no return
+*   @synchronous / Asynchronous : Synchronous
+*   @ Reentrant / Non Reentrant : Reentrant
+*/
 void WiFi_joinAccessPoint(uint8* SSID, uint8* pass, WIFI_SSD_Connection_Type * pRresult);
 
+/*
+*   @brief : this function used to tell you current state for network connection 
+*   @arg1  pStatus : will carry current state that can be any state from this defined data type (WIFI_AP_Status_Type)
+*   @return: no return
+*   @synchronous / Asynchronous : Synchronous
+*   @ Reentrant / Non Reentrant : Reentrant
+*/
 void WiFi_checkConnection(WIFI_AP_Status_Type * pStatus);
 
+/*
+*   @brief : this function used to openSocket (connect to server )
+*   @arg1  Domain: pointer for array of characters for server domain like "api.thingspeak.com"
+*   @arg2  Port: pointer for array of characters for port of server like "80"
+*   @arg3  pRresult:  will carry result for this function that can be any state from this defined data type (WIFI_Socket_Result_Type)
+*   @return: no return
+*   @synchronous / Asynchronous : Synchronous
+*   @ Reentrant / Non Reentrant : Reentrant
+*/
 void WiFi_start(uint8* Domain, uint8* Port, WIFI_Socket_Result_Type * pRresult );
 
+/*
+*   @brief : this function used to handle sending data for server (include inside it handling counting number of chacters will be sent)
+*   @arg1  data: pointer for array of characters for "way of how update in  server " + "value "
+*   Like                        "GET /update?api_key=TKVS1WL2NHLMCXDH&field2="  +  "150"
+*   @arg2 pRresult:  will carry result for this function that can be any state from this defined data type (WIFI_SEND_Result_Type)
+*   @return: no return
+*   @synchronous / Asynchronous : Synchronous
+*   @ Reentrant / Non Reentrant : Reentrant
+*/
 void WiFi_send(uint8* data,WIFI_SEND_Result_Type *pRresult);
 
 #endif
